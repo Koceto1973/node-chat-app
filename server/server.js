d49074eca04a025fb439d8970cc38b2da2ee44e4
TeamK <koceto1973@yahoo.ca>
2019-01-15 15:52:40 +0200

    broadcasting

--- server/server.js
+++ server/server.js
@@ -13,17 +13,34 @@ var io = socketIO(server);           // socket to server integration
 
 app.use(express.static(publicPath)); // static asset in by middleware
 
+// connection event triggers socket creation
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newEmail', {
-    from: 'mike@example.com',
-    text: 'Hey. What is going on.',
-    createAt: 123
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
   });
 
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
@@ -31,6 +48,7 @@ io.on('connection', (socket) => {
   });
 });
 
+// just the express app server
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
